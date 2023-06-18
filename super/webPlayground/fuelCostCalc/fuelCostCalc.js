f18368d2be74d34bf6e25f182ac0637b251e0d58
Jake Evans <jakeethanevans@gmail.com>
2023-06-18 16:59:25 +0100

    reformatted

--- super/webPlayground/fuelCostCalc/fuelCostCalc.js
+++ super/webPlayground/fuelCostCalc/fuelCostCalc.js
@@ -1,3 +1,16 @@
+// Event listeners to calculate the cost whenever an input changes
+document.getElementById("riders").addEventListener("input", calculateCost);
+document.getElementById("distance").addEventListener("input", calculateCost);
+document.getElementById("efficiency").addEventListener("input", calculateCost);
+document.getElementById("fuelPrice").addEventListener("input", calculateCost);
+document
+	.getElementById("distanceUnit")
+	.addEventListener("change", calculateCost);
+document
+	.getElementById("efficiencyUnit")
+	.addEventListener("change", calculateCost);
+document.getElementById("fuelPrice")
+// Create subroutine
 function calculateCost() {
 	// Get input values
 	var numRiders = parseFloat(document.getElementById("riders").value);
@@ -6,8 +19,17 @@ function calculateCost() {
 	var fuelPrice = parseFloat(document.getElementById("fuelPrice").value);
 
 	// Convert distance to miles if it's in kilometers
-	if (document.getElementById("unit").value === "km") {
+	if (document.getElementById("distanceUnit").value === "km") {
 		distance *= 0.621371;
+	}
+
+	// If Paying Passengers is empty, assume they mean 1 passenger (themself)
+	if (document.getElementById("riders").value === "") {
+		numRiders = 1;
+	}
+
+	if (document.getElementById("efficiencyUnit").value === "mpl") {
+		efficiency *= 0.22;
 	}
 
 	// Calculate fuel cost
@@ -19,8 +41,8 @@ function calculateCost() {
 
 	// Display the result
 	document.getElementById("result").innerHTML =
-		"The cost of the trip is: £" + costInPounds.toFixed(2);
+		"Total cost: £" + costInPounds.toFixed(2);
 
 	document.getElementById("result2").innerHTML =
-		"Cost Per Person: £" + costPerPerson.toFixed(2);
+		"Per passenger cost: £" + costPerPerson.toFixed(2);
 }
