7d7fee4693d0071372ad69bf51463a54b2241a31
Jake Evans <jakeethanevans@gmail.com>
2023-06-18 12:31:28 +0100

    idek

--- super/webPlayground/fuelCostCalc/fuelCostCalc.js
+++ super/webPlayground/fuelCostCalc/fuelCostCalc.js
@@ -1,9 +1,9 @@
 function calculateCost() {
 	// Get input values
+	var numRiders = parseFloat(document.getElementById("riders").value);
 	var distance = parseFloat(document.getElementById("distance").value);
 	var efficiency = parseFloat(document.getElementById("efficiency").value);
 	var fuelPrice = parseFloat(document.getElementById("fuelPrice").value);
-	var numRiders = parseFloat(document.getElementById("riders").value);
 
 	// Convert distance to miles if it's in kilometers
 	if (document.getElementById("unit").value === "km") {
@@ -15,12 +15,12 @@ function calculateCost() {
 	var litresUsed = gallonsUsed * 4.54609;
 	var costInPence = litresUsed * fuelPrice;
 	var costInPounds = costInPence / 100;
-    var costPerPerson = costInPounds / numRiders
+	var costPerPerson = costInPounds / numRiders;
 
 	// Display the result
 	document.getElementById("result").innerHTML =
 		"The cost of the trip is: £" + costInPounds.toFixed(2);
 
-    document.getElementById("result2").innerHTML =
+	document.getElementById("result2").innerHTML =
 		"Cost Per Person: £" + costPerPerson.toFixed(2);
 }
