8d313dfdf974f8ebe3da16f61c3af08e27ef8740
Jake Evans <jakeethanevans@gmail.com>
2023-10-13 21:13:41 +0100

    asd

---
+++ super/webPlayground/cps_test/cpsTest.js
@@ -0,0 +1,32 @@
+var divElement = document.getElementById("cps-box");
+var countDisplay = document.getElementById("countDisplay");
+
+var clickCount = 0; // Initialize the click count
+var timerActive = false; // To track if the timer is active
+
+function startTimer() {
+	if (!timerActive) {
+		timerActive = true;
+		clickCount = 1; // Reset the click count when the timer starts
+		countDisplay.textContent = clickCount;
+		setTimeout(function () {
+			timerActive = false;
+			// The 5-second timer has ended
+			alert(
+				"5 second timer ended. You clicked " +
+					clickCount +
+					" times in 5 seconds."
+			);
+		}, 5000);
+	}
+}
+
+function myFunction() {
+	if (timerActive) {
+		clickCount += 1;
+		countDisplay.textContent = clickCount;
+	}
+}
+
+divElement.addEventListener("click", myFunction);
+divElement.addEventListener("click", startTimer);
